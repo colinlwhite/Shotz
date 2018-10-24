9c337f63ea43391648153764fa56c2ee5f596e93
Colin White <clwhite814@gmail.com>
2018-10-23 20:45:27 -0500

    cards are printing ; still need to do some bootstrap work

--- javascripts/components/locationComponent.js
+++ javascripts/components/locationComponent.js
@@ -3,11 +3,16 @@
 const printLocals = (arrayOfLocals) => {
     let domString = '';
     arrayOfLocals.forEach((local) => {
-        domString += `<h1>${local.Image}</h1>
-        <h1>${local.Name}</h1>
-        <h1>${local.Address}</h1>
-        <h1>${local.Time}</h1>
-        `;
+      domString += `
+        <div class="card d-inline-block m-4" style="width: 18rem;">
+           <img class="card-img-top" src="${local.Image}" alt="Card image cap">
+           <div class="card-body text-center">
+              <h3 class="card-text">${local.Name}</h3>
+              <h3 class="card-text">${local.Address}</h3>
+              <h3 class="card-text">${local.Time}</h3>
+           </div>
+        </div> 
+          `; 
 })
     $("#locationDiv").append(domString);
 };
