d1c0e4254301966bbff0e25118bf96eafaaa4907
Colin White <clwhite814@gmail.com>
2018-11-05 20:56:38 -0600

    wip

--- javascripts/components/locationComponent.js
+++ javascripts/components/locationComponent.js
@@ -1,10 +1,32 @@
 // Writing to the dom for the location components
 
+const shootTimeClass = (shootTime) => {
+    let selectedClass = '';
+    switch (shootTime) {
+      case 'Morning': 
+        selectedClass = 'btn-warning'; 
+        break;
+      case 'Afternoon': 
+        selectedClass = 'btn-primary'; 
+        break;
+      case 'Evening': 
+        selectedClass = 'btn-success'; 
+        break;
+      case 'AfterDark': 
+        selectedClass = 'btn-danger'; 
+        break;
+      default: 
+        selectedClass = '';
+    }
+    return selectedClass;
+}
+
+
 const printLocals = (arrayOfLocals) => {
     let domString = '';
     arrayOfLocals.forEach((local) => {
       domString += `
-        <div class="card d-inline-block m-4" style="width: 18rem;" id="${local.Time}">
+        <div class="card d-inline-block m-4" style="width: 18rem; ${shootTimeClass(local.Time)}">
            <img class="card-img-top" src="${local.Image}" alt="Card image cap">
            <div class="card-body text-center">
               <h3 class="card-text">${local.Name}</h3>
@@ -16,32 +38,6 @@ const printLocals = (arrayOfLocals) => {
 })
     $("#locationDiv").append(domString);
 };
-
-$("#morningButton").on('click', (e) => {
-  console.log(e); // Making sure the click logs out 
-  // $("h4").filter(":contains('Afternoon')").remove(); // Removes the h4 text only 
-  //$("#Afternoon").remove(); // Only removes one card or so I thought
-  $("#Afternoon, #AfterDark, #Evening").detach();
-}); 
-
-$("#afternoonButton").on('click', (e) => {
-  $("#Morning, #AfterDark, #Evening").hide();
-  $("#afternoonButton").show();
-}); 
-
-$("#eveningButton").on('click', (e) => {
-  $("#Afternoon, #AfterDark, #Morning").remove();
-}); 
-
-$("#afterDarkButton").on('click', (e) => {
-  $("#Afternoon, #Morning, #Evening").remove();
-}); 
-
-// const sortLocals = () => {
-//   if ()
-// }
-
-
 
 export {printLocals}
 
--- javascripts/events.js
+++ javascripts/events.js
@@ -1,1 +1,39 @@
 // Attaching all event listeners
+
+const allButton = $('#all'); 
+const morningButton = $('#morning'); 
+const afternoonButton = $('#afternoon'); 
+const eveningButton = $('#evening'); 
+const afterDarkButton = $('#afterDark'); 
+
+const allButtonEvent = () => {
+    allButton.on('click', () => {
+        
+    });
+};
+
+
+$("#morningButton").on('click', (e) => {
+    console.log(e); // Making sure the click logs out 
+    // $("h4").filter(":contains('Afternoon')").remove(); // Removes the h4 text only 
+    //$("#Afternoon").remove(); // Only removes one card or so I thought
+    $("#Afternoon, #AfterDark, #Evening").detach();
+  }); 
+  
+
+  $("#afternoonButton").on('click', () => {
+    $("#Morning, #AfterDark, #Evening").hide();
+    $("#afternoonButton").show();
+  }); 
+  
+
+  $("#eveningButton").on('click', () => {
+    $("#Afternoon, #AfterDark, #Morning").remove();
+  }); 
+  
+
+  $("#afterDarkButton").on('click', () => {
+    $("#Afternoon, #Morning, #Evening").remove();
+  }); 
+  
+
